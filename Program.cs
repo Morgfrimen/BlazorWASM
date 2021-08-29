53acf878b621461d7cd46ca8cec858f47721e39f
Morgfrimen <44734894+Morgfrimen@users.noreply.github.com>
2021-08-29 22:46:43 +0300

    fix build

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ builder.RootComponents.Add<App>("#app");
 builder.Services.AddScoped(sp =>
 new HttpClient
 {
-	BaseAddress = new Uri(builder.HostEnvironment.BaseAddress + "/BlazorWASM")
+	BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
 }
 );
 
